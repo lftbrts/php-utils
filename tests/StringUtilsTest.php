b5b2687c0657ac286e11fdbdc1d0fcd711c266d5
Ulf Tiburtius <ulf@idea-works.de>
2015-04-13 08:07:24 +0200

    add string utils

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Ideaworks\Tests;
+
+use Ideaworks\Utils\Str as StringUtils;
+
+/**
+ * @author Ulf Tiburtius <ulf@idea-works.de>
+ * @since April 12, 2015
+ */
+class StringUtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAreEqualReverse()
+    {
+        $this->assertTrue(StringUtils::areEqualReverse('password', 'drowssap'));
+        $this->assertTrue(StringUtils::areEqualReverse('Password', 'drowssap', false));
+        $this->assertFalse(StringUtils::areEqualReverse('Розовая Пантера', 'Розовая Пантера'));
+    }
+
+    public function testAreEqual()
+    {
+        $this->assertTrue(StringUtils::areEqual('paul', 'paul'));
+        $this->assertFalse(StringUtils::areEqual('paul', 'PAUL'));
+
+        $this->assertTrue(StringUtils::areEqual('PAuL', 'paul', false));
+        $this->assertTrue(StringUtils::areEqual('paul', 'PAUL', false));
+    }
+
+    public function testMaxLength()
+    {
+        $this->assertTrue(StringUtils::maxLength('paul', 4));
+        $this->assertFalse(StringUtils::maxLength('paul', 1));
+        $this->assertFalse(StringUtils::maxLength('pinkpanther', 5));
+    }
+
+    public function testMinLength()
+    {
+        $this->assertTrue(StringUtils::minLength('paul', 4));
+        $this->assertTrue(StringUtils::minLength('pinkpanther', 5));
+        $this->assertFalse(StringUtils::minLength('paul', 5));
+    }
+
+    public function testIsEmpty()
+    {
+        $this->assertTrue(StringUtils::isEmpty("   "));
+        $this->assertTrue(StringUtils::isEmpty(null));
+        $this->assertTrue(StringUtils::isEmpty(''));
+        $this->assertFalse(StringUtils::isEmpty(' pink panther '));
+    }
+}
